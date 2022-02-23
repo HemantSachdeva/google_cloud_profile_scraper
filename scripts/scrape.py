6b89ca9262e2aa516c589e28d1d0280570c583d2
Hemant Sachdeva <hemant.evolver@gmail.com>
2022-02-23 19:33:22 +0530

    Update badges

--- scripts/scrape.py
+++ scripts/scrape.py
@@ -1,9 +1,18 @@
+from re import sub
+
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
 data = pd.read_csv("data/input.csv")
 rows = []
+
+VALID_BADGES = [
+    "Google Cloud Essentials",
+    "Flutter Essentials",
+    "Flutter Development",
+    "Dart Essentials"
+]
 
 for row in data.itertuples():
     name = row.name
@@ -16,9 +25,13 @@ for row in data.itertuples():
     d = {"name": name, "badges": ""}
     for index, item in enumerate(badges):
         title = badges[index].getText().replace("\n", "")
-        d["badges"] += f"{title}, "
+        if title in VALID_BADGES:
+            d["badges"] += f"{title}, "
 
     rows.append(d)
+
+for row in rows:
+    row["badges"] = sub(", $", "", row["badges"])  # remove trailing comma
 
 header = ["Name", "Badges"]
 df = pd.DataFrame.from_dict(rows)
